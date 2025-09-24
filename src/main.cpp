d4c4336b5f0ceb5367f7db4b631294b154b7e425
Nathan Josue <njosue1017@gmail.com>
2025-09-23 20:31:54 -0400

    Drive Creation

--- src/main.cpp
+++ src/main.cpp
@@ -73,22 +73,32 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+// Instantiate Motor Groups and Controller
+ pros::MotorGroup leftMotors({1, 2, 3, 4});
+ pros::MotorGroup rightMotors({5, 6, 7, 8});
+ pros::Controller controller;
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	// Set Motor Brakes to Coast
+	leftMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_COAST);
+	rightMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_COAST);
+	while(true){
+		int leftY = controller.get_analog(ANALOG_LEFT_Y);
+		int rightY = controller.get_analog(ANALOG_RIGHT_Y);
+		// Added Dead Zone for Both Motors
+		if(abs(leftY) > 3){
+			leftMotors.move(leftY);
+		} else{
+			leftMotors.move(0);
+		}
 
-
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		if(abs(rightY) > 3){
+			rightMotors.move(rightY);
+		} else{
+			rightMotors.move(0);
+		}
+		// Delay Added to Prevent Crashing
+		pros::delay(20);
 	}
 }
